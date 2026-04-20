152ea3aa19e4723779f3bbcc38cb8989a6ec7d98
Raju2473 <powerpulseapp5@gmail.com>
2026-04-20 12:36:15 +0530

    Update db.ts

--- lib/config/db.ts
+++ lib/config/db.ts
@@ -17,16 +17,18 @@
 
 import mongoose from 'mongoose';
 
-
-let isConnected = false;
-
-const connectDB = async () => {
-  if (isConnected) return;
   const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable');
 }
+
+
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) return;
+
 
   try {
     const db = await mongoose.connect(MONGODB_URI);
